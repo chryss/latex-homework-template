b3e6fab438b8e44b86d2e8a3746ef05c375d3de5
Chris Waigl <chris.waigl@gi.alaska.edu>
2012-09-10 11:59:50 -0800

    initial commit: LaTeX homework template for GEOS F631

---
+++ square.m
@@ -0,0 +1,12 @@
+function y = square(x)
+
+%  calculate the square of the given number 'x'
+%  
+%  Arguments:
+%  
+%       x           (input)   value to be squared
+%       y           (output)  the result of the square
+
+y = x*x;
+
+% end of square function
